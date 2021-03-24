0c522c5cd85b91e7551429348d9474c2a172f606
anujrawatcode <anujrawatcode@gmail.com>
2021-03-24 22:59:17 +0530

    practice

--- Practice/6.Count/lex.yy.c
+++ Practice/6.Count/lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,9 +360,9 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[10] =
+static const flex_int16_t yy_accept[9] =
     {   0,
-        2,    2,    4,    2,    3,    1,    2,    1,    0
+        0,    0,    5,    3,    4,    1,    2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -373,14 +373,14 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    3,    3,    3,
         3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        1,    1,    1,    1,    1,    1,    4,    4,    4,    4,
 
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,29 +397,29 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[4] =
+static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    2,    1
+        1,    1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[11] =
+static const flex_int16_t yy_base[9] =
     {   0,
-        0,    0,    6,    0,    7,    2,    0,    0,    7,    3
+        0,    0,    5,    6,    6,    6,    6,    6
     } ;
 
-static const flex_int16_t yy_def[11] =
+static const flex_int16_t yy_def[9] =
     {   0,
-        9,    1,    9,   10,    9,   10,   10,    6,    0,    9
+        8,    1,    8,    8,    8,    8,    8,    0
     } ;
 
 static const flex_int16_t yy_nxt[11] =
     {   0,
-        4,    5,    6,    7,    8,    9,    3,    9,    9,    9
+        4,    5,    6,    7,    8,    3,    8,    8,    8,    8
     } ;
 
 static const flex_int16_t yy_chk[11] =
     {   0,
-        1,    1,    1,   10,    6,    3,    9,    9,    9,    9
+        1,    1,    1,    1,    3,    8,    8,    8,    8,    8
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -436,10 +436,10 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "Prime.l"
-#line 2 "Prime.l"
-    #include<stdio.h>
-    int m;
+#line 1 "Count.l"
+#line 2 "Count.l"
+#include<stdio.h>
+int dig=0, cha=0;
 #line 444 "lex.yy.c"
 #line 445 "lex.yy.c"
 
@@ -658,7 +658,7 @@ YY_DECL
 		}
 
 	{
-#line 6 "Prime.l"
+#line 6 "Count.l"
 
 
 #line 665 "lex.yy.c"
@@ -688,13 +688,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 9 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 7 );
+		while ( yy_base[yy_current_state] != 6 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -720,40 +720,25 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 8 "Prime.l"
-{
-          int flag=0;
-          int m = atoi(yytext);
-          if(m==0 || m==1)
-            flag=1;
-          if(m==2)
-            flag=0;
-          else
-          {
-              for(int i=2; i<m; i++)
-                 if(m%i==0)
-                 {
-                     flag=1;
-                 }
-          }
-
-          if(flag == 0)
-            printf("Yes");
-          else
-            printf("Not");
-       }
+#line 8 "Count.l"
+{dig++;}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 30 "Prime.l"
-{ printf("wrong input"); }
+#line 10 "Count.l"
+
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 32 "Prime.l"
+#line 12 "Count.l"
+{cha++;}
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 14 "Count.l"
 ECHO;
 	YY_BREAK
-#line 757 "lex.yy.c"
+#line 742 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1050,7 +1035,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 9 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1078,11 +1063,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 9 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 8);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1758,13 +1743,18 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 32 "Prime.l"
+#line 14 "Count.l"
 
 
 int yywrap()
-{return 1;}
+{
+    return 1;
+}
 
 int main()
 {
+    yyin = fopen("input.txt", "r");
     yylex();
+    printf("\n digit = %d \n", dig);
+    printf("S char = %d \n", cha);
 }
